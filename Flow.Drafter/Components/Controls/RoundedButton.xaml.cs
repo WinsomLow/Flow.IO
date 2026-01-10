9811feed653d75646a4ffbfbcad4bc778a995195
Wen Siu <winsom94923@gmail.com>
2026-01-10 17:36:06 +0800

    layering the software for plugin mechanical

--- Flow.Drafter/Components/Controls/RoundedButton.xaml.cs
+++ Flow.Drafter/Components/Controls/RoundedButton.xaml.cs
@@ -1,7 +1,7 @@
 using System.Windows;
 using System.Windows.Controls;
 
-namespace FlowDrafter.Controls
+namespace Flow.Drafter.Controls
 {
     public partial class RoundedButton : UserControl
     {
